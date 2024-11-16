e20fd5f75b56311174c6e1dec1639a470f530172
Kentaro Barnes <kentaro.barnes5@gmail.com>
2024-11-16 14:46:34 -0800

    stuff

---
+++ src/gptController.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const openai_1 = __importDefault(require("openai"));
+const openai = new openai_1.default();
+const test_img = "../car parking 2.jpg";
---
+++ src/gptController.ts
@@ -0,0 +1,7 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI();
+
+const test_img: string = "../car parking 2.jpg";
+
+async function 
